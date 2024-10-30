66207ba63e9dd9b810450de3b1233848a3e9ba32
mcjwsn <maciek.wisniewski04@gmail.com>
2024-10-30 21:37:53 +0100

    cwiczenia 30.10

--- 3.0.sql
+++ 3.0.sql
@@ -5,7 +5,34 @@
 --select * from Sales
 --select * from Buyers
 
---select buyer_name, sales.buyer_id, prod_id, qty from buyers, sales where  buyers.buyer_id = sales.buyer_id--select buyer_name, s.buyer_id, prod_id, qty from buyers as b, sales as s where b.buyer_id = s.buyer_id--select buyer_name, s.buyer_id, prod_id, qty from buyers b inner join sales s on b.buyer_id = s.buyer_id--select buyer_name, sales.buyer_id, prod_id, qty from buyers inner join sales on buyers.buyer_id = sales.buyer_id-- baza Northwind--select p.ProductID, s.SupplierID, p.supplierid companyname from products p inner join suppliers s on p.supplierid = s.supplierid order by p.ProductID--select * from Products where productid = 1--select * from Suppliers where supplierid = 1--select * from Products where productid = 10--select * from Suppliers where supplierid = 4--Napisz polecenie zwracające jako wynik nazwy klientów, którzy złożyli zamówienia po 01 marca 1998 (baza northwind)--select distinct customers.customerid, companyname from orders inner join customers on orders.customerid = customers.customerid where orderdate > '1998-03-01'--baza joindb--select buyer_name, s.buyer_id, qty from buyers b left outer join sales s on b.buyer_id = s.buyer_id--Napisz polecenie zwracające wszystkich klientów z datami zamówień (baza northwind).--select companyname, customers.customerid, orderdate from customers left outer join orders on customers.customerid = orders.customerid where orderid is null--1.Wybierz nazwy i ceny produktów (baza northwind) o cenie jednostkowej pomiędzy 20.00 a 30.00, dla każdego produktu podaj dane adresowe dostawcy
+--select buyer_name, sales.buyer_id, prod_id, qty from buyers, sales where  buyers.buyer_id = sales.buyer_id
+--select buyer_name, s.buyer_id, prod_id, qty from buyers as b, sales as s where b.buyer_id = s.buyer_id
+
+--select buyer_name, s.buyer_id, prod_id, qty from buyers b inner join sales s on b.buyer_id = s.buyer_id
+--select buyer_name, sales.buyer_id, prod_id, qty from buyers inner join sales on buyers.buyer_id = sales.buyer_id
+
+
+-- baza Northwind
+
+--select p.ProductID, s.SupplierID, p.supplierid companyname from products p inner join suppliers s on p.supplierid = s.supplierid order by p.ProductID
+--select * from Products where productid = 1
+--select * from Suppliers where supplierid = 1
+--select * from Products where productid = 10
+--select * from Suppliers where supplierid = 4
+
+
+--Napisz polecenie zwracające jako wynik nazwy klientów, którzy złożyli zamówienia po 01 marca 1998 (baza northwind)
+
+--select distinct customers.customerid, companyname from orders inner join customers on orders.customerid = customers.customerid where orderdate > '1998-03-01'
+
+--baza joindb
+
+--select buyer_name, s.buyer_id, qty from buyers b left outer join sales s on b.buyer_id = s.buyer_id
+
+--Napisz polecenie zwracające wszystkich klientów z datami zamówień (baza northwind).
+--select companyname, customers.customerid, orderdate from customers left outer join orders on customers.customerid = orders.customerid where orderid is null
+
+--1.Wybierz nazwy i ceny produktów (baza northwind) o cenie jednostkowej pomiędzy 20.00 a 30.00, dla każdego produktu podaj dane adresowe dostawcy
 --select od.orderid, od.UnitPrice, orders.ShipAddress from [order details] od inner join orders on od.orderid = orders.orderid where unitprice between 20 and 30
 
 --2. Wybierz nazwy produktów oraz inf. o stanie magazynu dla produktów dostarczanych przez firmę ‘Tokyo Tradersʼ
@@ -14,9 +41,86 @@
 --select * from products where SupplierID=4
 
 --3. Czy są jacyś klienci którzy nie złożyli żadnego zamówienia w 1997 roku, jeśli tak to pokaż ich dane adresowe
-select CustomerID, CompanyName, Address from Customers where CustomerID not in (select distinct CustomerID from Orders where year(OrderDate) = 1997)
+--select CustomerID, CompanyName, Address from Customers where CustomerID not in (select distinct CustomerID from Orders where year(OrderDate) = 1997)
 --select distinct CustomerID from Orders where year(OrderDate) = 1997
-select customers.customerid, companyname, customers.address from customers left join orders on customers.customerid = orders.customerid and year(orderdate) = 1997 where orders.orderid is null;
---4. Wybierz nazwy i numery telefonów dostawców, dostarczających produkty, których aktualnie nie ma w magazynie.
---5. Wybierz zamówienia złożone w marcu 1997. Dla każdego takiego zamówienia wyświetl jego numer, datę złożenia zamówienia oraz nazwę i numer telefonu klienta
+--select customers.customerid, companyname, customers.address from customers left join orders on customers.customerid = orders.customerid and year(orderdate) = 1997 where orders.orderid is null;
+--select customers.CompanyName,address,max(country) -- count(1) jesli wiemy ze nie bedzie zlych
+--from customers left join orders on customers.CustomerID = orders.CustomerID and year(orders.orderdate) = 1997
+--group by customers.CompanyName, customers.CustomerID,address-- musi byc klucz zeby nie nadpisaly sie powtorzeniahaving count(orderid) = 0
 
+--select customers.CompanyName, orders.orderid from customers left join orders on customers.CustomerID = orders.CustomerID and year(orderdate)=1997
+--where orders.orderid is null
+
+
+
+--4. Wybierz nazwy i numery telefonów dostawców, dostarczających produkty, których aktualnie nie ma w magazynie.
+--select CompanyName, Phone from suppliers join products on suppliers.SupplierID = PRODUCTs.supplierid  where products.unitsinstock = 0
+--select supplierid from products where UnitsInStock=0
+--select supplierid,CompanyName from suppliers
+
+--5. Wybierz zamówienia złożone w marcu 1997. Dla każdego takiego zamówienia wyświetl jego numer, datę złożenia zamówienia oraz nazwę i numer telefonu klienta
+--select customers.customerid, companyname,phone, orders.OrderDate,orderid from customers join orders on customers.CustomerID = orders.CustomerID WHERE year(orders.OrderDate) = 1997 and month(orders.OrderDate) = 3
+
+--baza library
+--1. Napisz polecenie, które wyświetla listę dzieci będących członkami biblioteki (baza library). Interesuje nas imię, nazwisko i data urodzenia dziecka.
+--select birth_date, member.lastname, member.firstname from juvenile join member on member.member_no = juvenile.member_no
+
+--2. Napisz polecenie, które podaje tytuły aktualnie wypożyczonych książek
+--select title from title join loan on loan.title_no = title.title_no
+
+--3. Podaj informacje o karach zapłaconych za przetrzymywanie książki o tytule ‘Tao TehKingʼ. Interesuje nas data oddania książki, ile dni była przetrzymywana i jaką zapłacono karę
+--select in_date, day(in_date-due_date), fine_assessed from loanhist join title on loanhist.title_no = title.title_no where title.title = 'Tao Teh King'
+--select in_date, month(in_date)-month(due_date), fine_assessed from loanhist join title on loanhist.title_no = title.title_no where title.title = 'Tao Teh King'
+--4. Napisz polecenie które podaje listę książek (mumery ISBN) zarezerwowanych przezosobę o nazwisku: Stephen A. Graff
+--select isbn from reservation join member on member.member_no = reservation.member_no where member.lastname = 'Graff' and member.firstname = 'Stephen' and member.middleinitial = 'A'
+--select * from member
+
+--baza Northwind
+--1. Wybierz nazwy i ceny produktów (baza northwind) o cenie jednostkowej pomiędzy 20.00 a 30.00, dla każdego produktu podaj dane adresowe dostawcy, interesują nas tylko produkty z kategorii ‘Meat/Poultryʼ
+--select productName, UnitPrice, Suppliers.CompanyName, Suppliers.Address from Products join Categories on Products.CategoryID = Categories.CategoryID join Suppliers on Products.SupplierID = Suppliers.SupplierID where Categories.CategoryName = 'Meat/Poultry' and Products.UnitPrice between 20 and 30
+
+--2. Wybierz nazwy i ceny produktów z kategorii ‘Confectionsʼ dla każdego produktu podaj nazwę dostawcy.
+--select productName, UnitPrice, Suppliers.CompanyName, categories.categoryName from Products join Categories on Products.CategoryID = Categories.CategoryID join Suppliers 
+--on Products.SupplierID = Suppliers.SupplierID where Categories.CategoryName = 'Confections'
+
+--3. Dla każdego klienta podaj liczbę złożonych przez niego zamówień. Zbiór wynikowy powinien zawierać nazwę klienta, oraz liczbę zamówień
+--select customers.CompanyName,count(orderid) from customers join orders on customers.CustomerID = orders.CustomerID group by customers.CompanyName
+
+--4. Dla każdego klienta podaj liczbę złożonych przez niego zamówień w marcu 1997r
+--select customers.CompanyName,count(orderid) from customers join orders on customers.CustomerID = orders.CustomerID where month(orders.orderdate) = 3 group by customers.CompanyName
+
+--1. Który ze spedytorów był najaktywniejszy w 1997 roku, podaj nazwę tego spedytora
+--2. Dla każdego zamówienia podaj wartość zamówionych produktów. Zbiór wynikowy powinien zawierać nr zamówienia, datę zamówienia, nazwę klienta oraz wartość zamówionych produktów
+--3. Dla każdego zamówienia podaj jego pełną wartość (wliczając opłatę za przesyłkę). Zbiór wynikowy powinien zawierać nr zamówienia, datę zamówienia, nazwę klienta oraz pełną wartość zamówienia
+
+--3. Wybierz nazwy i numery telefonów klientów, którzy kupowali produkty z kategorii‘Confectionsʼ
+--. Wybierz nazwy i numery telefonów klientów, którzy nie kupowali produktów z kategorii‘Confectionsʼ
+--. Wybierz nazwy i numery telefonów klientów, którzy w 1997r nie kupowali produktów zkategorii ‘Confectionsʼ
+
+--1. Napisz polecenie, które wyświetla listę dzieci będących członkami biblioteki (bazalibrary). Interesuje nas imię, nazwisko, data urodzenia dziecka i adres zamieszkaniadziecka.
+--select juvenile.member_no, concat(firstname,' ',lastname) imie_nazw, birth_date, concat(city,' ', street) adres, adult.state, adult.street from juvenile join member on juvenile.member_no = member.member_no    join adult on juvenile.adult_member_no = adult.member_no;
+--2. Napisz polecenie, które wyświetla listę dzieci będących członkami biblioteki (bazalibrary). Interesuje nas imię, nazwisko, data urodzenia dziecka, adres zamieszkaniadziecka oraz imię i nazwisko rodzica
+--select juvenile.member_no, birth_date, d.firstname, d.lastname, concat(city,' ', street) adres, m.firstname, m.lastname from juvenile join member d on juvenile.member_no = d.member_no  join adult on juvenile.adult_member_no = adult.member_no join member m on adult.member_no = m.member_no order by m.member_no
+
+--1. Napisz polecenie, które wyświetla pracowników oraz ich podwładnych (bazanorthwind)
+--select employees.Lastname,employees.firstname,,e.FirstName,e.LastName from employees join employees e on employees.EmployeeID = e.ReportsTo
+--2. Napisz polecenie, które wyświetla pracowników, którzy nie mają podwładnych (bazanorthwind)
+--3. Napisz polecenie, które wyświetla pracowników, którzy mają podwładnych (bazanorthwind)
+-- do poprawy
+--select distinct e.EmployeeID, Employees.Lastname,employees.firstname,e.FirstName,e.LastName from employees left  join employees e on employees.EmployeeID = e.ReportsTo where e.FirstName is not Null
+--1. Podaj listę członków biblioteki mieszkających w Arizonie (AZ) mają więcej niż dwojedzieci zapisanych do biblioteki
+--2. Podaj listę członków biblioteki mieszkających w Arizonie (AZ) którzy mają więcej niżdwoje dzieci zapisanych do biblioteki oraz takich którzy mieszkają w Kaliforni (CA) imają więcej niż troje dzieci zapisanych do biblioteki
+
+--dla kazdego klienta podaj liczbe zamowien w 1997
+--select customers.CompanyName,count(orderid) as 'Liczba zamowien w 1997' -- count(1) jesli wiemy ze nie bedzie zlych
+--from customers left join orders on customers.CustomerID = orders.CustomerID 
+--and year(orders.orderdate) = 1997
+--group by customers.CompanyName, customers.CustomerID-- musi byc klucz zeby nie nadpisaly sie powtorzenia
+--having count(orderid) = 0
+
+--dla kazdego klienta podaj wartosc zamowien w 1997 dodaj oplaty za przesylke
+--select customers.CompanyName,count(orderid) as 'Liczba zamowien w 1997' 
+--from customers left join orders on customers.CustomerID = orders.CustomerID  and year(orders.orderdate) = 1997 
+--group by customers.CompanyName, customers.CustomerID
+-- dla kazdego klienta podja wartosc zakupioncyh przez niego produktow z kategorii confections
+-- dla kazdego klienta podaj liczbe roznych prod z kategorii confections zakupioncyh przez niego w 1997
--- sgl_2.1.sql
+++ sgl_2.1.sql
@@ -4,7 +4,13 @@
 --select top 10 OrderID, (unitprice*quantity)*(1-discount) as 'value' from [order details] order by value desc
 --3. Podaj nr zamówienia oraz wartoœæ zamówienia, dla zamówieñ, dla których ³¹czna liczba zamawianych jednostek produktów jest wiêksza ni¿ 250
 --select orderid, sum(Quantity * UnitPrice * (1 - Discount)) AS OrderValue from[Order Details] gROUP BY OrderID HAVING SUM(Quantity) > 250;
--- 4.Podaj liczbê zamówionych jednostek produktów dla produktów, dla których productid jest mniejszy ni¿ 3--select productid,sum(Quantity) from [Order Details] group by productid having productid<3--ZMIANA BAZY--ponizsze zadanie jest zepsute--1. Ilu jest doros³ych czytekników
+-- 4.Podaj liczbê zamówionych jednostek produktów dla produktów, dla których productid jest mniejszy ni¿ 3
+--select productid,sum(Quantity) from [Order Details] group by productid having productid<3
+
+--ZMIANA BAZY
+
+--ponizsze zadanie jest zepsute
+--1. Ilu jest doros³ych czytekników
 --select count(member_no) as 'Liczba' from juvenile where AGE(birthdate)>=INTERVAL '18 years'  --postregsql
 --select count(member_no) as 'Liczba' from juvenile where MONTHS_BETWEEN(SYSDATE, BirthDate) / 12 >= 18 --oracle
 --SELECT COUNT(member_no) AS AdultReadersCount FROM juvenile WHERE DATEDIFF(YEAR, Birth_Date, GETDATE())  - CASE WHEN MONTH(Birth_Date) > MONTH(GETDATE()) OR (MONTH(Birth_Date) = MONTH(GETDATE()) AND DAY(Birth_Date) > DAY(GETDATE())) THEN 1 ELSE 0 END >= 18;
@@ -22,10 +28,23 @@
 --3. Dla ka¿dego czytelnika podaj liczbê ksi¹¿ek zwróconych przez niego w 2001r.
 --select member_no, count(isbn) from (select member_no, isbn, in_date from loanhist where year(in_date)=2001) as subquery group by member_no
 --4. Dla ka¿dego czytelnika podaj sumê kar jakie zap³aci³ w 2001r
-select member_no, sum(fine_paid) from (select member_no, fine_paid from loanhist where year(in_date)=2001 and fine_paid is not null) as subquery group by member_no
+--select member_no, sum(fine_paid) from (select member_no, fine_paid from loanhist where year(in_date)=2001 and fine_paid is not null) as subquery group by member_no
 --5. Ile ksi¹¿ek wypo¿yczono w maju 2001
---6. Na jak d³ugo œrednio by³y wypo¿yczane ksi¹¿ki w maju 2001--1. Dla ka¿dego pracownika podaj liczbê obs³ugiwanych przez niego zamówieñ w 1997r
+--select count(out_date) from loanhist where year(out_date) = 2001  and month(out_date) = 5
+--6. Na jak d³ugo œrednio by³y wypo¿yczane ksi¹¿ki w maju 2001
+--select avg(day(in_date - out_date)) from loanhist where year(out_date)=2001  and month(out_date) = 2001
+
+--1. Dla ka¿dego pracownika podaj liczbê obs³ugiwanych przez niego zamówieñ w 1997r\
+--select count(EmployeeID) as 'Liczba zamowien' from Orders where year(Orderdate)=1997  group by EmployeeID
 --2. Dla ka¿dego pracownika podaj ilu klientów (ró¿nych klientów) obs³ugiwa³ ten pracownik w 1997r
+--select count(distinct customerid) from orders where year(Orderdate)=1997 group by employeeid
 --3. Dla ka¿dego spedytora/przewoŸnika podaj ³¹czn¹ wartoœæ "op³at za przesy³kê" dla przewo¿onych przez niego zamówieñ
---4. Dla ka¿dego spedytora/przewoŸnika podaj ³¹czn¹ wartoœæ "op³at za przesy³kê" przewo¿onych przez niego zamówieñ w latach od 1996 do 1997--1. Dla ka¿dego pracownika podaj liczbê obs³ugiwanych przez niego zamówieñ z podzia³em na lata
+--select shipVia, sum(freight) as 'suma' from orders group by shipvia
+--4. Dla ka¿dego spedytora/przewoŸnika podaj ³¹czn¹ wartoœæ "op³at za przesy³kê" przewo¿onych przez niego zamówieñ w latach od 1996 do 1997
+--select shipVia, sum(freight) as 'suma' from orders where year(shippeddate) between 1996 and 1997 group by shipvia 
+
+
+--1. Dla ka¿dego pracownika podaj liczbê obs³ugiwanych przez niego zamówieñ z podzia³em na lata
+select EmployeeID,year(orderdate) as Years,count(orderid) from orders group by employeeid,year(orderdate) order by EmployeeID, Years
 --2. Dla ka¿dego pracownika podaj liczbê obs³ugiwanych przez niego zamówieñ z podzia³em na lata i miesi¹ce.
+select EmployeeID,year(orderdate) as Years,month(orderdate) as months, count(orderid) from orders group by employeeid,year(orderdate),month(orderdate) order by EmployeeID, Years, months
